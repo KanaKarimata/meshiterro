6f099bc0498e024c41e379aba06e41de14348ceb
KanaKarimata <kana.lt22@gmail.com>
2022-04-07 01:49:11 +0000

    [Finish]4-14

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  # get 'users/show'
+  # get 'users/edit'
   # get 'postimages/new'
   # get 'postimages/index'
   # get 'postimages/show'
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:new, :create, :index, :show,:destroy, :edit]
   devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
