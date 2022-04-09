c8b0d723c1b2fa08681091d26069f2fca74f7c98
KanaKarimata <kana.lt22@gmail.com>
2022-04-09 13:32:32 +0000

    [Finish]4-24

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,13 +1,14 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
-  belongs_to :post_image
+
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
+
   validates :shop_name, presence: true
   validates :image, presence: true
-  
+
   def get_image
    unless image.attached?
      file_path = Rials.root.join('app/assets/image/no_image.jpg')
@@ -15,9 +16,9 @@ class PostImage < ApplicationRecord
    end
    image
   end
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+ devise_for :users
   # get 'users/show'
   # get 'users/edit'
   # get 'postimages/new'
@@ -8,11 +9,11 @@ Rails.application.routes.draw do
    resource :favorites, only: [:create, :destroy]
    resources :post_comments, only: [:create, :destroy]
  end
-  
+
   resources :users, only: [:show, :edit, :update]
-  
-  
-  devise_for :users
+
+
+
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
